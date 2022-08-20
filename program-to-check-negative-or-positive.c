35e56f3870d51e36707fd4b363be78f27ac1ed27
Amit Naskar <64121569+Amitn2003@users.noreply.github.com>
2022-08-20 14:48:52 +0530

    Program to check negative or positive.
    Write a program in Java to check whether given no. is negative or positive.

---
+++ program-to-check-negative-or-positive.c
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main () {
+  //Write a program in Java to check whether given no. is negative or positive.
+  cout << "Enter the number"<< endl;
+  int n;
+  cin >> n;
+  if (n > 0)
+    {
+      cout << "Positive number!!";
+    }
+  else if (n < 0)
+    {
+    cout << "Negative number!!";
+    }
+    else {
+    cout << "0000000000000000";
+    }
+  return 0;
+}
