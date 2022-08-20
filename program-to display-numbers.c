8f83e687ffa5476dd53e65b92b6dca9d19206cf9
Amit Naskar <64121569+Amitn2003@users.noreply.github.com>
2022-08-20 14:52:23 +0530

    Write a program in Java to display from 20 to 0

---
+++ program-to display-numbers.c
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+  //Write a program in Java to display from 20 to 0
+  for(int a = 20; a>=0; a--) {
+      cout << a <<endl;}
+  return 0;
+}
