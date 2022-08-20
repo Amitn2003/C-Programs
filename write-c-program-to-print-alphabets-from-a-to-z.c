7be5beedd656420c9d69b97d78bbbb1c4c89e0ad
Amit Naskar <64121569+Amitn2003@users.noreply.github.com>
2022-08-20 14:37:40 +0530

    Write C program to print alphabets from a to z

---
+++ write-c-program-to-print-alphabets-from-a-to-z.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    // Write C program to print alphabets from a to z
+    for (char i = 'a'; i <= 'z'; i++) {
+        printf("%c", i);
+    }
+}
