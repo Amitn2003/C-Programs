07a9e45079b228fa83294d684e13c55bab9747c0
Amit Naskar <64121569+Amitn2003@users.noreply.github.com>
2022-08-20 14:51:13 +0530

    Program to display the natural no
    Write a program in Java to display the natural no. from 1 to 50 [for loop]

---
+++ program-to-display-the-natural-no.c
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+  //Write a program in Java to display the natural no. from 1 to 50 [for loop]
+  for (int i = 1; i<=50; i++){
+      cout<<i<<endl;}
+  return 0;
+}
