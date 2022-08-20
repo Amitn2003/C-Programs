4103ac7c3e77f84ed6c14785afaa447aaed901b1
Amit Naskar <64121569+Amitn2003@users.noreply.github.com>
2022-08-20 14:47:58 +0530

    Program to check Odd Even
    Write a program  Java to check whether the no. is odd or even.

---
+++ Program-java-to-check-whether-the-no.-is-odd-or-even.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Write a program  Java to check whether the no. is odd or even.
+    cout<<"Enter the number: ";
+    int a;
+    cin>>a;
+    if (a%2==0) {
+        cout<<"Even";
+    }
+    else {
+        cout<<"Odd";
+    }
+    return 0;
+}
