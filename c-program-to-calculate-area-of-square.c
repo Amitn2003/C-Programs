4da989e1e4e3eefb8b82efa19391ffa8fbf202a8
Amit Naskar <64121569+Amitn2003@users.noreply.github.com>
2022-08-20 14:34:02 +0530

    C Program to Calculate Area of Square

---
+++ c-program-to-calculate-area-of-square.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    //C Program to Calculate Area of Square
+    int n;
+    printf("Enter one side: ");
+    scanf("%d", &n);
+    printf("The area is %d units.", n * n);
+    
+}
